e3d1be192e8f7286d14735cd84004d666ba5ef13
jhoward9009 <j.how9009@gmail.com>
2022-07-08 01:37:59 -0400

    user

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27000/social-backend', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.set('debug', true)
+
+  app.use(require('./routes/api'))
+
+  app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
+
